6f2138af196933522aac81be3b346d71a11d7c9d
Franta Mejta <frantisek.mejta@studentagency.cz>
2015-04-29 16:19:41 +0200

    Fixes #3: Consider only latest naginator-rescheduled downstream run

--- src/main/java/com/dabsquared/gitlabjenkins/CommitAction.java
+++ src/main/java/com/dabsquared/gitlabjenkins/CommitAction.java
@@ -24,8 +24,10 @@
 package com.dabsquared.gitlabjenkins;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.SortedSet;
@@ -41,8 +43,15 @@ import org.kohsuke.stapler.HttpResponse;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 
-import lombok.Data;
+import com.chikli.hudson.plugin.naginator.NaginatorCause;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import hudson.Plugin;
+import hudson.model.Cause;
+import hudson.model.CauseAction;
 import hudson.model.Fingerprint;
 import hudson.model.Fingerprint.RangeSet;
 import hudson.model.Job;
@@ -52,6 +61,7 @@ import hudson.plugins.git.util.BuildData;
 import hudson.security.ACL;
 import hudson.tasks.Fingerprinter.FingerprintAction;
 import hudson.util.HttpResponses;
+import hudson.util.ReflectionUtils;
 
 import jenkins.model.Jenkins;
 import jenkins.security.NotReallyRoleSensitiveCallable;
@@ -62,6 +72,7 @@ import jenkins.security.NotReallyRoleSensitiveCallable;
  * @author Franta Mejta
  * @sa.date 2015-03-09T13:15:09+0100
  */
+@Slf4j
 public class CommitAction {
 
     private static final Comparator<Run<?, ?>> RUN_COMPARATOR = new Comparator<Run<?, ?>>() {
@@ -142,24 +153,34 @@ public class CommitAction {
         for (final FingerprintAction fa : run.getActions(FingerprintAction.class)) {
             for (final Fingerprint fp : fa.getFingerprints().values()) {
                 for (final Map.Entry<String, RangeSet> e : fp.getUsages().entrySet()) {
-                    final Job<?, ?> fpjob = ACL.impersonate(ACL.SYSTEM, new NotReallyRoleSensitiveCallable<Job<?, ?>, RuntimeException>() {
-
-                        private static final long serialVersionUID = 1L;
-
-                        @Override
-                        public Job<?, ?> call() {
-                            return Jenkins.getActiveInstance().getItemByFullName(e.getKey(), Job.class);
-                        }
-
-                    });
-
+                    final Job<?, ?> fpjob = ACL.impersonate(ACL.SYSTEM, new JobByNameCallable(e.getKey()));
                     if (fpjob != null) {
+                        final List<Run<?, ?>> fpruns = new ArrayList<Run<?, ?>>();
                         for (final int build : e.getValue().listNumbers()) {
                             final Run<?, ?> fprun = fpjob.getBuildByNumber(build);
                             if (fprun != null) {
-                                if (list.add(fprun)) {
-                                    this.findDownstreamRuns(list, fprun);
+                                fpruns.add(run);
+                            }
+                        }
+
+                        /* #3: Consider only latest naginator-rescheduled downstream run */
+                        final Plugin naginator = Jenkins.getActiveInstance().getPlugin("naginator");
+                        if (naginator != null && naginator.getWrapper().isActive()) {
+                            for (final Iterator<Run<?, ?>> i = fpruns.iterator(); i.hasNext();) {
+                                final Run<?, ?> fprun = i.next();
+                                for (final Run<?, ?> remaining : fpruns) {
+                                    final Integer ngc = this.getNaginatorCause(remaining);
+                                    if (ngc != null && ngc.equals(fprun.getNumber())) {
+                                        i.remove();
+                                        break;
+                                    }
                                 }
+                            }
+                        }
+
+                        for (final Run<?, ?> fprun : fpruns) {
+                            if (list.add(fprun)) {
+                                this.findDownstreamRuns(list, fprun);
                             }
                         }
                     }
@@ -168,6 +189,36 @@ public class CommitAction {
         }
 
         return list;
+    }
+
+    @Nullable
+    private Integer getNaginatorCause(@Nonnull final Run<?, ?> run) {
+        for (final CauseAction ca : run.getActions(CauseAction.class)) {
+            for (final Cause c : ca.getCauses()) {
+                if (c instanceof NaginatorCause) {
+                    try {
+                        final Field field = ReflectionUtils.findField(c.getClass(), "summary");
+                        ReflectionUtils.makeAccessible(field);
+
+                        final String summary = String.valueOf(field.get(c));
+                        if (summary.startsWith("#")) {
+                            return Integer.valueOf(summary.substring(1));
+                        } else {
+                            log.debug("summary field in NaginatorCause does not "
+                                      + "contain build number. value={}", summary);
+                        }
+                    } catch (final SecurityException ex) {
+                        log.warn("cannot access field in NaginatorCause", ex);
+                    } catch (final ReflectiveOperationException ex) {
+                        log.warn("cannot access field in NaginatorCause", ex);
+                    } catch (final NumberFormatException ex) {
+                        log.warn("summary field in NaginatorCause has unexpected value", ex);
+                    }
+                }
+            }
+        }
+
+        return null;
     }
 
     public void doDynamic(final StaplerRequest req, final StaplerResponse rsp) throws IOException, ServletException {
@@ -199,6 +250,20 @@ public class CommitAction {
         @JsonProperty("status")
         public final String getStatusText() {
             return this.getStatus().asText();
+        }
+
+    }
+
+    @RequiredArgsConstructor
+    public static final class JobByNameCallable extends NotReallyRoleSensitiveCallable<Job<?, ?>, RuntimeException> {
+
+        private static final long serialVersionUID = 1L;
+        @Nonnull
+        private final String jobName;
+
+        @Override
+        public Job<?, ?> call() {
+            return Jenkins.getActiveInstance().getItemByFullName(this.jobName, Job.class);
         }
 
     }
